3947aa7663f127f66350de8f35dc0f600484a7f8
ntijoh-theo-hiort <theo.hiort@elev.ga.ntig.se>
2023-11-09 10:47:12 +0100

    created necessary views

---
+++ src/components/searchBarComponent.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+    
+</script>
+
+<template>
+    <form>
+        <input id="text-input" type="text">
+        <input id="submit-input" type="submit" value="🔎">
+    </form>
+</template>
+
+<style scoped>
+    form{
+        position: sticky;
+    }
+
+    #text-input{
+        font-size: large;
+        height: 40px;
+        width: 90vw;
+    }
+
+    #submit-input{
+        height: 40px;
+        width: 2vw;
+    }
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,18 @@
 import { createApp } from 'vue'
 import './style.css'
+import {createRouter, createWebHashHistory} from 'vue-router'
 import App from './App.vue'
+import reposView from './views/reposView.vue'
+import forksView from './views/forksView.vue'
 
-createApp(App).mount('#app')
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes: [
+        {path: '/repos', component: reposView},
+        {path: '/:repo/forks', component: forksView}
+    ]
+})
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
